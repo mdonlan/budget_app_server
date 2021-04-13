de078171c93a88b4b08cc3e22d3e764429d7dad0
Michael Donlan <me@michaeldonlan.com>
2021-04-13 15:44:40 -0400

    working on login

--- src/index.js
+++ src/index.js
@@ -13,13 +13,6 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
-
-// const client = new Client()
-// await client.connect()
-// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-// console.log(res.rows[0].message) // Hello world!
-// await client.end()
-
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -27,59 +20,6 @@ const pool = new Pool({
   password: 'password',
   // port: 5432,
 })
-
-// const name = "test transactions 01";
-// pool.query('INSERT INTO transactions (Name) VALUES ($1)', [name], (error, results) => {
-//     if (error) {
-//         throw error
-//     }
-//     console.log("inserted new transaction");
-// })
-
-// pool.query('SELECT * FROM legislators', (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     console.log(results.rows)
-//   })
-// const name = "blah blah";
-// pool.query('INSERT INTO legislators (Name) VALUES ($1)', [name], (error, results) => {
-//     if (error) {
-//         throw error
-//     }
-//     console.log("inserted new legislator");
-//     // response.status(201).send(`User added with ID: ${result.insertId}`)
-// })
-
-// async function get_legislators() {
-//     try {
-//         const queryArguments = {
-//             apikey: '22691594f640230fd8ca371699406559',
-//             output: 'json'
-//         }
-    
-//         const response = await superagent.get('https://www.opensecrets.org/api/?method=getLegislators&id=ME').query(queryArguments)
-//         const json = JSON.parse(response.text);
-
-//         console.log(json.response.legislator)
-
-//         console.log('\n\n\n');
-
-//         console.log(json.response.legislator["@attributes"])
-
-//         // pool.query('INSERT INTO legislators (Name) VALUES ($1)', [response.text], (error, results) => {
-//         //     if (error) {
-//         //         throw error
-//         //     }
-//         //     console.log("inserted new legislator");
-//         //     // response.status(201).send(`User added with ID: ${result.insertId}`)
-//         // })
-        
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 
 app.get('/', async (req, res) => {
 	console.log('root request')
@@ -100,7 +40,7 @@ async function get_user_transactions() {
 
 app.post('/register_user', function(req, res) {
   console.log('register user')
-  // console.log(req.body);
+  console.log(req.body);
 
   pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [req.body.email, req.body.username, req.body.password], (error, results) => {
       if (error) {
@@ -125,4 +65,71 @@ app.post('/register_user', function(req, res) {
 
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);
+})
+
+app.post('/login', function(req, res) {
+  console.log('login')
+  console.log(req.body);
+
+  pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [req.body.data.username, req.body.data.password], (error, results) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("row_count: " + results.rowCount);
+        console.log("rows: \n", results.rows);
+        if (results.rowCount > 0) {
+          // res.status(200).send({valid_token: valid_login, username: })
+          const token = jwt.sign({ username: req.body.username }, 'private_key');
+          res.status(200).send({ message: `Logged in successfully!`, token: token });
+        } else {
+          res.status(401).send({message: 'Failed to login'})
+        }
+        // if (results.rows[0].username == decoded.username) {
+            // res.status(200).send({ valid_token: true, username: decoded.username });
+        // }
+        // res.status(200).json(results.rows);
+    }
+})
+
+  // pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [req.body.email, req.body.username, req.body.password], (error, results) => {
+  //     if (error) {
+  //         console.log(error);
+  //         if (error.constraint == 'unique_email') {
+  //             res.status(422).send('Error -- Email has already been registered');
+  //         } else if (error.constraint == 'unique_username') {
+  //             res.status(422).send('Error -- Username has already been registered');
+              
+  //         } else if (error.code == '23502') {
+  //             res.status(422).send('Error -- A field was left empty, please fill all fields!');
+  //         }  else {
+  //             res.status(400).send("Unknown error during registration");
+  //         }
+  //     } else {
+  //         // console.log(results);
+  //         const token = jwt.sign({ username: req.body.username }, 'private_key');
+  //         res.status(201).send({ message: `Registered user successfully!`, token: token });
+  //     }
+  // })
+})
+
+app.post('/validate_token', function(req, res) {
+  // console.log(req.body);
+
+  const decoded = jwt.verify(req.body.token, 'private_key');
+  // console.log(decoded)
+
+  pool.query('SELECT * FROM users WHERE username = $1', [decoded.username], (error, results) => {
+      if (error) {
+          console.log(error);
+      } else {
+          // console.log(results);
+          if (results.rowCount > 0) {
+            if (results.rows[0].username == decoded.username) {
+              res.status(200).send({ valid_token: true, username: decoded.username });
+            }
+          }
+          
+          // res.status(200).json(results.rows);
+      }
+  })
 })
