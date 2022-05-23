cc9a95de7e0b6ede34359fb575c1431128bbab95
Michael Donlan <mdonlan8792@yahoo.com>
2022-05-22 23:29:23 -0400

    added expenses and num_transactions, added date-fns

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const superagent = require('superagent');
 const { Client } = require('pg')
 const Pool = require('pg').Pool;
 const jwt = require('jsonwebtoken');
+const start_of_week = require('date-fns/startOfWeek');
+const end_of_week = require('date-fns/endOfWeek');
+
+var types = require('pg').types
+types.setTypeParser(1700, function (val) {
+    return parseFloat(val);
+});
 
 const app = express();
 app.use(cors())
@@ -14,24 +21,24 @@ app.use(bodyParser.json());
 const port = 3000;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'budget_app',
-  password: 'password',
-  // port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'budget_app',
+    password: 'password',
+    // port: 5432,
 })
 
 app.post('/create_transaction', (req, res) => {
-  const date = new Date();
-  const transaction = req.body.transaction;
-  const username = jwt.decode(req.body.token, 'private_key').username;
+    const date = new Date();
+    const transaction = req.body.transaction;
+    const username = jwt.decode(req.body.token, 'private_key').username;
 
-	pool.query('INSERT INTO transactions (name, date, tags, username, value) VALUES ($1, $2, $3, $4, $5)', [transaction.name, date, transaction.tags, username, transaction.value], (error, results) => {
-    if (error) console.log(error);
-      else {
-        res.send("created transaction successfully!")
-    }
-	})
+    pool.query('INSERT INTO transactions (name, date, tags, username, value) VALUES ($1, $2, $3, $4, $5)', [transaction.name, date, transaction.tags, username, transaction.value], (error, results) => {
+        if (error) console.log(error);
+        else {
+            res.send("created transaction successfully!")
+        }
+    })
 })
 
 // async function get_user_transactions() {
@@ -40,105 +47,105 @@ app.post('/create_transaction', (req, res) => {
 // 	return results.rows;
 // }
 
-app.post('/register_user', function(req, res) {
-  console.log('register user')
-//   console.log(req.body);
+app.post('/register_user', function (req, res) {
+    console.log('register user')
+    //   console.log(req.body);
 
-  const username = req.body.username;
-  const password = req.body.password;
-  const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
 
-  if (username.length == 0 || password.length == 0 || email.length == 0) {
-    res.status(200).send({ message: "Error -- Field was empty."});
-    return;
-  }
+    if (username.length == 0 || password.length == 0 || email.length == 0) {
+        res.status(200).send({ message: "Error -- Field was empty." });
+        return;
+    }
 
-  // check to make sure username is unique
-  // const users_with_name = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-  // if (users_with_name.rows.length > 0) {
-  //   res.status(200).send('Error -- Username is already taken.')
-  // }
+    // check to make sure username is unique
+    // const users_with_name = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    // if (users_with_name.rows.length > 0) {
+    //   res.status(200).send('Error -- Username is already taken.')
+    // }
 
-  pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [email, username, password], (error, results) => {
-      if (error) {
-          // console.log(error);
-          console.log(error.constraint)
-          if (error.constraint == 'unique_email') {
-              res.status(200).send({ message: 'Error -- Email has already been registered.' });
-          } else if (error.constraint == 'unique_username') {
-              res.status(200).send({ message: 'Error -- Username has already been registered.' });
-              
-          } else if (error.code == '23502') {
-              res.status(200).send({ message: 'Error -- A field was left empty, please fill all fields!'});
-          }  else {
-              res.status(200).send({ message: "Unknown error during registration"});
-          }
-      } else {
-          // console.log(results);
-          const token = jwt.sign({ username: username }, 'private_key');
-          res.status(201).send({ message: `Registered user successfully!`, token: token });
-      }
-  })
+    pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [email, username, password], (error, results) => {
+        if (error) {
+            // console.log(error);
+            console.log(error.constraint)
+            if (error.constraint == 'unique_email') {
+                res.status(200).send({ message: 'Error -- Email has already been registered.' });
+            } else if (error.constraint == 'unique_username') {
+                res.status(200).send({ message: 'Error -- Username has already been registered.' });
+
+            } else if (error.code == '23502') {
+                res.status(200).send({ message: 'Error -- A field was left empty, please fill all fields!' });
+            } else {
+                res.status(200).send({ message: "Unknown error during registration" });
+            }
+        } else {
+            // console.log(results);
+            const token = jwt.sign({ username: username }, 'private_key');
+            res.status(201).send({ message: `Registered user successfully!`, token: token });
+        }
+    })
 })
 
 app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 })
 
-app.post('/login', function(req, res) {
-  console.log('login')
-//   console.log(req.body);
+app.post('/login', function (req, res) {
+    console.log('login')
+    //   console.log(req.body);
 
-  const username = req.body.username;
-  const password = req.body.password;
-//   console.log(username, password)
+    const username = req.body.username;
+    const password = req.body.password;
+    //   console.log(username, password)
 
-  
 
-  pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (error, results) => {
-    if (error) {
-        console.log(error);
-        res.status(401).send({message: 'Failed to login'})
-    } else {
-        // console.log("row_count: " + results.rowCount);
-        // console.log("rows: \n", results.rows);
-        if (results.rowCount > 0) {
-          // res.status(200).send({valid_token: valid_login, username: })
-          const token = jwt.sign({ username: username }, 'private_key');
-          res.status(200).send({ message: `Logged in successfully!`, token: token });
+
+    pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (error, results) => {
+        if (error) {
+            console.log(error);
+            res.status(401).send({ message: 'Failed to login' })
         } else {
-          res.status(401).send({message: 'Failed to login'})
-        }
-        // if (results.rows[0].username == decoded.username) {
+            // console.log("row_count: " + results.rowCount);
+            // console.log("rows: \n", results.rows);
+            if (results.rowCount > 0) {
+                // res.status(200).send({valid_token: valid_login, username: })
+                const token = jwt.sign({ username: username }, 'private_key');
+                res.status(200).send({ message: `Logged in successfully!`, token: token });
+            } else {
+                res.status(401).send({ message: 'Failed to login' })
+            }
+            // if (results.rows[0].username == decoded.username) {
             // res.status(200).send({ valid_token: true, username: decoded.username });
-        // }
-        // res.status(200).json(results.rows);
-    }
-})
+            // }
+            // res.status(200).json(results.rows);
+        }
+    })
 
-  // pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [req.body.email, req.body.username, req.body.password], (error, results) => {
-  //     if (error) {
-  //         console.log(error);
-  //         if (error.constraint == 'unique_email') {
-  //             res.status(422).send('Error -- Email has already been registered');
-  //         } else if (error.constraint == 'unique_username') {
-  //             res.status(422).send('Error -- Username has already been registered');
-              
-  //         } else if (error.code == '23502') {
-  //             res.status(422).send('Error -- A field was left empty, please fill all fields!');
-  //         }  else {
-  //             res.status(400).send("Unknown error during registration");
-  //         }
-  //     } else {
-  //         // console.log(results);
-  //         const token = jwt.sign({ username: req.body.username }, 'private_key');
-  //         res.status(201).send({ message: `Registered user successfully!`, token: token });
-  //     }
-  // })
+    // pool.query('INSERT INTO users (email, username, password) VALUES ($1, $2, $3)', [req.body.email, req.body.username, req.body.password], (error, results) => {
+    //     if (error) {
+    //         console.log(error);
+    //         if (error.constraint == 'unique_email') {
+    //             res.status(422).send('Error -- Email has already been registered');
+    //         } else if (error.constraint == 'unique_username') {
+    //             res.status(422).send('Error -- Username has already been registered');
+
+    //         } else if (error.code == '23502') {
+    //             res.status(422).send('Error -- A field was left empty, please fill all fields!');
+    //         }  else {
+    //             res.status(400).send("Unknown error during registration");
+    //         }
+    //     } else {
+    //         // console.log(results);
+    //         const token = jwt.sign({ username: req.body.username }, 'private_key');
+    //         res.status(201).send({ message: `Registered user successfully!`, token: token });
+    //     }
+    // })
 })
 
 app.post('/validate_token', async (req, res) => {
-  console.log('attempting to validate token')
+    console.log('attempting to validate token')
 
     const token = req.body.token;
     if (!token) {
@@ -148,7 +155,7 @@ app.post('/validate_token', async (req, res) => {
 
     // if there was a token, check it
     const decoded = jwt.verify(req.body.token, 'private_key');
-  
+
     await pool.query('SELECT * FROM users WHERE username = $1', [decoded.username], (error, results) => {
         if (error) console.log(error);
         else {
@@ -162,11 +169,11 @@ app.post('/validate_token', async (req, res) => {
         }
     })
 })
-app.post('/delete_transaction', async function(req, res) {
-  if (!req.body.token) {
-		res.send('no username');
-		return;
-	}
+app.post('/delete_transaction', async function (req, res) {
+    if (!req.body.token) {
+        res.send('no username');
+        return;
+    }
 
     /*
         delete_transaction
@@ -175,10 +182,10 @@ app.post('/delete_transaction', async function(req, res) {
         3. delete the transaction in db
         4. update the category amount to reverse the transaction
     */
-	
+
     const transaction_id = req.body.transaction_id;
     const username = jwt.decode(req.body.token, 'private_key').username;
-    
+
     const transaction_query = await pool.query('SELECT * FROM transactions WHERE username = $1 AND id = $2', [username, transaction_id]);
     const transaction_data = transaction_query.rows[0];
 
@@ -189,9 +196,9 @@ app.post('/delete_transaction', async function(req, res) {
     const transaction_category = transaction_data.category;
 
     const category_name = transaction_data.category;
-	const category_data = await pool.query('SELECT * FROM categories WHERE username = $1 AND name = $2', [username, category_name]);
-	const current_category_amount = parseFloat(category_data.rows[0].current_amount);
-	const new_category_amount = current_category_amount - transaction_amount;
+    const category_data = await pool.query('SELECT * FROM categories WHERE username = $1 AND name = $2', [username, category_name]);
+    const current_category_amount = parseFloat(category_data.rows[0].current_amount);
+    const new_category_amount = current_category_amount - transaction_amount;
 
     pool.query('DELETE FROM transactions WHERE username = $1 AND name = $2 AND id = $3', [username, transaction_data.name, transaction_data.id], (error, results) => {
         if (error) console.log(error);
@@ -202,13 +209,13 @@ app.post('/delete_transaction', async function(req, res) {
     })
 
     pool.query('UPDATE categories SET current_amount = $1 WHERE username = $2 AND name = $3', [new_category_amount, username, category_name], (error, results) => {
-		if (error) console.log(error);
-		else console.log('updated the associated category amount')
-	})
+        if (error) console.log(error);
+        else console.log('updated the associated category amount')
+    })
 
 })
 
-app.post('/get_transactions', function(req, res) {
+app.post('/get_transactions', function (req, res) {
     const decoded = jwt.verify(req.body.token, 'private_key');
 
     pool.query('SELECT * FROM transactions WHERE username = $1', [decoded.username], (error, results) => {
@@ -218,3 +225,115 @@ app.post('/get_transactions', function(req, res) {
         }
     })
 })
+
+app.post('/get_month_data', function (req, res) {
+    const decoded = jwt.verify(req.body.token, 'private_key');
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const days_in_month = new Date(year, month, 0).getDate() + 1;
+    const start = new Date(year, month);
+    const end = new Date(year, month, days_in_month);
+
+    pool.query('SELECT * FROM transactions WHERE username = $1 AND date BETWEEN $2 AND $3', [decoded.username, start, end], (error, results) => {
+        if (error) console.log(error);
+        else {
+            res.status(200).send({ transactions: results.rows });
+        }
+    })
+})
+
+app.post('/get_week_data', function (req, res) {
+    const decoded = jwt.verify(req.body.token, 'private_key');
+
+    const date = new Date;
+    const first = date.getDate() - date.getDay(); // First day is the day of the month - the day of the week
+    const last = first + 6; // last day is the first day + 6  
+
+    const first_day = new Date(date.setDate(first));
+    const last_day = new Date(date.setDate(last));
+    // console.log("first: " + first_day);
+    // console.log("last: " + last_day);
+
+    pool.query('SELECT * FROM transactions WHERE username = $1 AND date BETWEEN $2 AND $3', [decoded.username, first_day, last_day], (error, results) => {
+        if (error) console.log(error);
+        else {
+            res.status(200).send({ transactions: results.rows });
+        }
+    })
+})
+
+app.post('/get_day_data', function (req, res) {
+    const decoded = jwt.verify(req.body.token, 'private_key');
+
+    const date = new Date;
+    // const first = date.getDate() - date.getDay(); // First day is the day of the month - the day of the week
+    // const last = first + 6; // last day is the first day + 6  
+
+    // const first_day = new Date(date.setDate(first));
+    // const last_day = new Date(date.setDate(last));
+    // console.log("first: " + first_day);
+    // console.log("last: " + last_day);
+
+    pool.query('SELECT * FROM transactions WHERE username = $1 AND date = $2', [decoded.username, date], (error, results) => {
+        if (error) console.log(error);
+        else {
+            res.status(200).send({ transactions: results.rows });
+        }
+    })
+})
+
+app.post('/get_transaction_numbers_data', async function(req, res) {
+    const username = jwt.verify(req.body.token, 'private_key').username;
+  
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const days_in_month = new Date(year, month, 0).getDate() + 1;
+    const month_start = new Date(year, month);
+    const month_end = new Date(year, month, days_in_month);
+
+    const first = date.getDate() - date.getDay(); // First day is the day of the month - the day of the week
+    const last = first + 6; // last day is the first day + 6  
+
+    // const first_day_week = new Date(date.setDate(first));
+    // const last_day_week = new Date(date.setDate(last));
+    
+    const first_day_week = start_of_week(date);
+    const last_day_week = end_of_week(date);
+
+    console.log("month_start " + month_start)
+    console.log("month_end " + month_end)
+    console.log("first_day_week " + first_day_week)
+    console.log("last_day_week " + last_day_week)
+
+
+    const transactions = await pool.query('SELECT * FROM transactions WHERE username = $1 AND date BETWEEN $2 AND $3', [username, month_start, month_end]);
+    // console.log(transactions.rows)
+    const rows = transactions.rows;
+
+    let num_month_transactions = 0;
+    let num_week_transactions = 0;
+    let num_day_transactions = 0;
+
+    for (let i = 0; i < rows.length; i++) {
+        const t_date = new Date(rows[i].date);
+        console.log("t_date: " + t_date)
+        if (t_date > month_start) num_month_transactions++;
+
+        if (t_date >= first_day_week && t_date <= last_day_week) num_week_transactions++;
+
+        if (t_date == date) num_day_transactions++;
+    }
+
+    console.log("num_month_transactions: " + num_month_transactions);
+    console.log("num_week_transactions: " + num_week_transactions);
+    console.log("num_day_transactions: " + num_day_transactions);
+
+    res.status(200).send({ 
+        num_month_transactions: num_month_transactions,
+        num_week_transactions: num_week_transactions,
+        num_day_transactions: num_day_transactions
+     }); 
+  })
